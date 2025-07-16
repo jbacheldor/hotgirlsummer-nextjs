94f1f6966ec9934ab9a61cbb0a1b6825e4f00f89
Jess <jlbachel@umich.edu>
2025-07-16 15:59:29 -0400

    redirect working !!

--- src/app/calendar/[month]/page.tsx
+++ src/app/calendar/[month]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import CalendarBody from "../../components/calender";
 import Stats from "@/app/components/stats";
 import styles from './style.module.css';
+import { redirect } from "next/navigation";
 
 type UrlStuff = {params: Promise<{
     month: string,
@@ -11,21 +12,7 @@ const Calendar:React.FC<UrlStuff> = async ({params}) => {
     const {month} = await params
     let today = new Date(Date.now());
 
-    return (
-        <p></p>
-        // <div id={styles.calendarpage}>
-        //     <div id="title-section">
-        //         <h1 id={styles.pagetitle}>Calendar</h1>
-        //         <hr/>
-        //     </div>
-        //     <CalendarBody month={month} year={year}/>
-        //     <Stats/>
-
-        //     <div id='footer-section'>
-        //         <Link href="/recommendation"><button>Recommendations</button></Link>
-        //     </div>
-        // </div>
-    )
+    redirect(`/calendar/${today.getMonth()}/${today.getFullYear()}`)
 }
 
 export default Calendar
--- src/app/components/calender.tsx
+++ src/app/components/calender.tsx
@@ -2,7 +2,7 @@
 import DateCircles from "./datecircles";
 
 type Props = {
-    month: number;
+    month: string;
     year: string;
 }
 
@@ -10,6 +10,25 @@ const CalendarBody: React.FC<Props> = ({month, year}) => {
     var months = [ "january", "february", "march", "april", "may", "june", 
             "july", "august", "september", "october", "november", "december" ];
     // let index = months.indexOf(month)
+    var month_num = parseInt(month)
+    var year_num = parseInt(year)
+
+    var today = new Date(Date.now())
+    var today_month = today.getMonth()
+    var today_year = today.getFullYear()
+
+    var next = false;
+    var prev = false;
+
+    // if the current year is greater than the one we are querying
+    // then we should be able to hit next 
+    if(today_year > year_num) next = true
+    // if it's the same year but the month is less than the current month
+    if(today_year == year_num && today_month > month_num) next = true
+
+    // prev is tricky b/c we only want to go back so far
+    // also we only want to show months w/ data 
+    if(today.getFullYear() <= year_num) prev = true
     
 
     var weeks = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -19,21 +38,21 @@ const CalendarBody: React.FC<Props> = ({month, year}) => {
     return (
             <div id="calendar-body">
                 <div id="month-nav">
-                    <h2>* {months[month-1]} {year} *</h2>
+                    <h2>* {months[month_num-1]} {year} *</h2>
                 </div>
                 <div id='circles-wrapper'>
+                    <div id="calendar-circles">
                     <div id="weekdays">
                         {weekDays.map((i, index)=> <span key={index}>{i}</span>)}
                         </div>
-                    <div id="calendar-circles">
-                        {[...Array(weeks[month-1])].map((i, index)=>
+                        {[...Array(weeks[month_num-1])].map((i, index)=>
                             <DateCircles {...{'day': index+1, 'event': true, 'id': '1234'}}/>
                         )}
                     </div>
                 </div>
                 <div id="month-nav">
-                    <button>prev</button>
-                    <button>next</button>
+                    <button disabled={!prev}>prev</button>
+                    <button disabled={!next}>next</button>
                 </div>
 
                 <style jsx>
@@ -51,8 +70,6 @@ const CalendarBody: React.FC<Props> = ({month, year}) => {
                         display: flex;
                         justify-content: center;
                         align-items: center;
-                        border: 1px solid white;
-                        border-radius: 5px;
                         flex-direction: column;
                     }
                     #month-nav h2{
@@ -83,9 +100,10 @@ const CalendarBody: React.FC<Props> = ({month, year}) => {
                     #calendar-circles {
                         display: flex;
                         flex-flow: row wrap;
-                        align-items: center;
-                        justify-content: center;
-                        width: 385px;
+                        padding: 0 5px;
+                        width: 400px;
+                        border: 1px solid white;
+                        border-radius: 5px;
                     }
                     `}
                 </style>
